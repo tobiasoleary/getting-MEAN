57b88c7fcdd688571d5cc8dbf9ba1a7849fb6d73
Tobias O'Leary <tobiasoleary@mac.com>
2018-01-12 18:58:41 -0500

    Updates api/locations with semi-working read request for location with id

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const mongoose = require('mongoose')
+const Location = mongoose.model('Location')
+
 function dev_SuccessResponse(res) {
 	res.status(200)
 	res.json(JSON.stringify({status: "SUCCESS"}))
@@ -8,23 +11,37 @@ function dev_SuccessResponse(res) {
 const locationsController = {
 
 	locationsListByDistance: (req, res) => {
-		tmpSuccessResponse(res)
+		dev_SuccessResponse(res)
 	},
 
 	locationsCreate: (req, res) => {
-		tmpSuccessResponse(res)
+		dev_SuccessResponse(res)
 	},
 
 	locationsReadOne: (req, res) => {
-		tmpSuccessResponse(res)
+		let locationId = req.params.locationId
+
+		Location.findById(locationId).exec((err, location) => {
+			if(err) {
+				console.log(`!!!ERROR finding location with id: ${locationId}.\t\n${err}`)
+				res.status(500)
+				res.json({'status': 'error'})
+			} else {
+				console.log(`Found Location using ID: ${locationId}`)
+				console.log(location)
+
+				res.status(200)
+				res.json(location)
+			}
+		})
 	},
 
 	locationsUpdateOne: (req, res) => {
-		tmpSuccessResponse(res)
+		dev_SuccessResponse(res)
 	},
 
 	locationsDeleteOne: (req, res) => {
-		tmpSuccessResponse(res)
+		dev_SuccessResponse(res)
 	}
 }
 
